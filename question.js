c3438b7f29dd12e24f9a9d3bacbe58df79d4a769
Mustafa <137828560+MustafaHuzaifa07@users.noreply.github.com>
2023-07-18 00:39:57 +0500

    Add files via upload

---
+++ question.js
@@ -0,0 +1,62 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Which planet is known as the Red Planet?",
+    answer: "Mars",
+    options: ["Mars", "Venus", "Jupiter", "Saturn"]
+  },
+  {
+    numb: 2,
+    question: "Who painted the Mona Lisa?",
+    answer: "Leonardo da Vinci",
+    options: ["Vincent van Gogh", "Leonardo da Vinci", "Pablo Picasso", "Michelangelo"]
+  },
+  {
+    numb: 3,
+    question: "What is the largest ocean on Earth?",
+    answer: "Pacific Ocean",
+    options: ["Atlantic Ocean", "Pacific Ocean", "Indian Ocean", "Arctic Ocean"]
+  },
+  {
+    numb: 4,
+    question: "Who discovered penicillin?",
+    answer: "Alexander Fleming",
+    options: ["Marie Curie", "Albert Einstein", "Isaac Newton", "Alexander Fleming"]
+  },
+  {
+    numb: 5,
+    question: "What is the chemical symbol for gold?",
+    answer: "Au",
+    options: ["Go", "Ag", "Au", "Ge"]
+  },
+  {
+    numb: 6,
+    question: "Who wrote the play 'Romeo and Juliet'?",
+    answer: "William Shakespeare",
+    options: ["William Shakespeare", "George Orwell", "Jane Austen", "Mark Twain"]
+  },
+  {
+    numb: 7,
+    question: "What is the national animal of India?",
+    answer: "Bengal Tiger",
+    options: ["Lion", "Elephant", "Bengal Tiger", "Peacock"]
+  },
+  {
+    numb: 8,
+    question: "What is the tallest mountain in the world?",
+    answer: "Mount Everest",
+    options: ["K2", "Kangchenjunga", "Mount Everest", "Makalu"]
+  },
+  {
+    numb: 9,
+    question: "Which country is home to the Eiffel Tower?",
+    answer: "France",
+    options: ["Italy", "Spain", "France", "Germany"]
+  },
+  {
+    numb: 10,
+    question: "What is the largest planet in our solar system?",
+    answer: "Jupiter",
+    options: ["Venus", "Saturn", "Jupiter", "Neptune"]
+  }
+];
